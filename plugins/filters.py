5e4d84f9d42c97b6e3330c9a2aa7608e34a97d9c
sathanxavier1998 <71935696+sathanxavier1998@users.noreply.github.com>
2021-03-23 15:19:50 +0530

    Update filters.py

--- plugins/filters.py
+++ plugins/filters.py
@@ -24,7 +24,7 @@ from config import MAINCHANNEL_ID
 
 BUTTONS = {}
  
-@Client.on_message(filters.group & filters.text)
+@Client.on_message(filters.group & filters.text & filters.bot)
 async def filter(client: Bot, message: Message):
     if re.findall("((^\/|^,|^!|^\.|^[\U0001F600-\U000E007F]).*)", message.text):
         return
